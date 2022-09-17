f5c119d4b917a235ca6f05ae62a820c5d72c51ac
Muwanguzi Silas <54215685+Silas-seal@users.noreply.github.com>
2022-09-17 22:16:16 +0300

    Create HelloJenkins.java

---
+++ src/HelloJenkins.java
@@ -0,0 +1,13 @@
+ /**
+ This is a Sample CD/CI Pipelines Java Application
+  */
+
+  public class HelloJenkins {
+    /**
+    This is the executing method (main)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins!");
+    }
+    
+  }
